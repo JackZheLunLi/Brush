f8ffd9a8203fa19be844f634b015a3c7d08dc4b7
curry123987 <curry123987@gmail.com>
2022-05-31 23:46:07 +0800

    5/31/1

--- app/src/main/java/com/jack/brushing/MainActivity.java
+++ app/src/main/java/com/jack/brushing/MainActivity.java
@@ -33,7 +33,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Acti//权限请求\n" +
+                                "    private RxPermissions rxPermissions;\n" +
+                                "    //是否拥有权限\n" +
+                                "    private boolean hasPermissions = false;\n" +
+                                "    //底部弹窗\n" +
+                                "    private BottomSheetDialog bottomSheetDialog;\n" +
+                                "    //弹窗视图\n" +
+                                "    private View bottomView;\n" +
+                                "    //存储拍完照后的图片\n" +
+                                "    private File outputImagePath;\n" +
+                                "    //启动相机标识\n" +
+                                "    public static final int TAKE_PHOTO = 1;\n" +
+                                "    //启动相册标识\n" +
+                                "    public static final int SELECT_PHOTO = 2;\n" +
+                                "    //图片控件\n" +
+                                "    private ShapeableImageView ivHead;\n" +
+                                "    //Base64\n" +
+                                "    private String base64Pic;\n" +
+                                "    //拍照和相册获取图片的Bitmap\n" +
+                                "    private Bitmap orc_bitmap;on", null).show();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -41,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_parentmodel, R.id.nav_set)
+                R.id.nav_home/*, R.id.nav_parentmodel*/, R.id.nav_set,R.id.nav_support,R.id.nav_about)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
